0b599cc6fa3463e9bb65df531704c3321680723c
mac <mac@MacBook-Pro-de-mac.local>
2025-12-05 01:31:11 -0600

    back terminado5

--- dev_sistema_escolar_api/settings.py
+++ dev_sistema_escolar_api/settings.py
@@ -54,7 +54,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 TEMPLATES = [
     {
